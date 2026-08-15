64d4350a16c55033957d81d71648e82f0dc3737d
Voult <adamrogaczewski20@gmail.com>
2026-08-15 17:42:40 +0200

    Update serwer.js

--- backend/serwer.js
+++ backend/serwer.js
@@ -2,11 +2,15 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const routes = require("./routes");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(routes);
 
 app.get("/api/health", (req, res) => {
   res.json({
