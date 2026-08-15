a3c1687c4ea1065ef28d878a5c762a2d00791d97
Voult <adamrogaczewski20@gmail.com>
2026-08-15 20:44:41 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,12 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL
+});
+
+pool.on("error", (error) => {
+    console.error("Błąd połączenia z bazą danych:", error);
 });
 
 module.exports = pool;
