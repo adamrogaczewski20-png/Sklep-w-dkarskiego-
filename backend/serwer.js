dae2079eefdc6e0d8d6a9990f2499bff244d768e
Voult <adamrogaczewski20@gmail.com>
2026-08-15 17:34:47 +0200

    Create serwer.js

---
+++ backend/serwer.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    status: "ok",
+    message: "Backend sklepu działa"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend działa na porcie ${PORT}`);
+});
